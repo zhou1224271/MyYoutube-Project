75751c46fbf32ea294da4df408dd0cf807d023a6
zSia <zhou1224271@gmail.com>
2018-07-05 22:19:45 +0900

    aa

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import requests
 
-print(dir(requests))
+from urllib.parse import parse_qs
+
+VID = "Fmdb-KmlzD8"
+
+def parse(qs):
+    d = parse_qs(qs)
+    m = d["url_encoded_fmt_stream_map"]
+    print(m)
+
+def main():
+    resp = requests.get('https://www.youtube.com/get_video_info?video_id={}'.format(VID))
+    parse(resp.content)
+
+if name == "main" :
+    main()
