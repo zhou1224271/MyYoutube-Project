d353b40cddd8975e010145e1159cdb5bcedbb3f1
zSia <zhou1224271@gmail.com>
2018-07-08 14:08:52 +0900

    c
    cc

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import requests
-
 from urllib.parse import parse_qs
+import shutil
 
 VID = "Fmdb-KmlzD8"
 
+
 def parse(qs):
     d = parse_qs(qs)
-    m = d["url_encoded_fmt_stream_map"]
-    print(m)
+    steam_lst = d["url_encoded_fmt_stream_map"]
+    d2 = parse_qs(steam_lst[0])
+    url = d2["url"]
+    print(url)
+    return url
+
 
 def main():
     resp = requests.get('https://www.youtube.com/get_video_info?video_id={}'.format(VID))
-    parse(resp.text)
+    url = parse(resp.text)[0]
+    res = requests.get(url)
+    with open("{}.mp4".format(VID), "wb") as file:
+        shutil.copyfileobj(res.raw, file)
 
-if name == "main" :
+if __name__ == "__main__":
     main()
